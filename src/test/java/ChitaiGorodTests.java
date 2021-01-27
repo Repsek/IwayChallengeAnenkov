0df0f7022f438956c278a69d2d125ad579a89cf2
Repsek <ianenkow@gmail.com>
2021-01-27 08:14:08 +0300

    Initial commit

---
+++ src/test/java/ChitaiGorodTests.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class ChitaiGorodTests {
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    @BeforeAll
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 5);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
+    }
+
+    
+
+
+
+
+}
